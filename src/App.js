8cc0d344fa45f99e372ac441699a9cd32230e05c
TakumiNara <takuminara0428@gmail.com>
2019-01-04 23:50:48 +0900

    Form design

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ class App extends Component {
     super(props)
 
     this.state = {
-      name: "",
+      address: "",
       body: "",
     }
 
-    this.onClick = this.onClick.bind(this);
+    this.onChangeAddress = this.onChangeAddress.bind(this);
+    this.onChangeBody = this.onChangeBody.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    //this.onClick = this.onClick.bind(this);
     // this.onClick();
   }
 
   onClick() {
-    console.log(db)
+    /*console.log(db)
     db.collection('contacts').limit(1).get().then((res) => {
       res.forEach((doc) => {
         const document = doc.data();
@@ -26,10 +29,25 @@ class App extends Component {
           body: document.body,
         }, () => { console.log(this.state.name) });
       })
-    })
+    })*/
+  }
+
+  onChangeAddress(e) {
+    this.setState({ address: e.target.value });
+  }
+
+  onChangeBody(e) {
+    this.setState({ body: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("Submit");
   }
 
   render() {
+    const { address, body } = this.state;
+
     return (
       <div>
         <div className="top">
@@ -64,6 +82,34 @@ class App extends Component {
               <p><a href="https://twitter.com/nyaratakumi">Twitter</a></p>
               <p><a href="https://www.facebook.com/nyaratakumi">Facebook</a></p>
               <p><a href="https://github.com/nyaratakumi">GitHub</a></p>
+              <br/>
+              <p>or</p>
+              <br/>
+              <form onSubmit={this.onSubmit}>
+                <div>
+                  <p>連絡先</p>
+                  <input
+                    type="text"
+                    className="form"
+                    placeholder="メールアドレス、電話番号"
+                    value={address}
+                    onChange={this.onChangeAddress}
+                  />
+                </div>
+                <div>
+                  <p>内容</p>
+                  <textarea
+                    className="form"
+                    rows="5"
+                    placeholder="お問い合わせ内容"
+                    value={body}
+                    onChange={this.onChangeBody}
+                  />
+                </div>
+                <div>
+                  <button type="submit" className="submit">送信</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
